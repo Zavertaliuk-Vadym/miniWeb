419bb215ec38e3a48d7db94d87bdb9e0c9bd7bfd
Вадимка <zavertal.v@gmail.com>
2016-11-10 00:00:15 +0200

    without calendar

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 public class Client {
     static String header = "<!DOCTYPE html>\n" +
@@ -12,8 +11,17 @@ public class Client {
             "<head>\n" +
             "<title>Page Title</title>\n" +
             "</head>\n" +
-            "<body>\n";
-    static String footer = "</body>\n" +
+            "<body>\n"+
+            "\n" +
+            " <form action=\"\">\n" +
+            "  <p><b>Введите вариант</b></p>\n" +
+            "  <input type=\"text\" name=\"name\" \n<Br>\n" +
+            "  <p><input type=\"submit\"></p>\n" +
+            " </form>\n" +
+            "";
+
+    static String footer =
+            "</body>\n" +
             "</html>";
 
     public static void main(String args[]) throws IOException, URISyntaxException {
@@ -22,22 +30,51 @@ public class Client {
             try (ServerSocket server = new ServerSocket(8080)) {
                 System.out.println("Listening for connection on port 8080 ....");
                 StringBuilder builder = new StringBuilder();
-
-
                 Socket socket = server.accept();
                 builder.append(header);
                 requestNumber++;
                 String httpResponse = "" + "Hello world! Times: " + requestNumber;
-
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                System.out.println(Arrays.toString(reader.readLine().split("/")));
-                builder.append(httpResponse);
+                String parameters = reader.readLine().split(" ")[1];
+                if (parameters.equals("/?")) {
+//                    builder.append(getViwe());
+                    System.out.println(parameters);
+                    builder.append("Hello World!");
+                } else {
+                    builder.append("Hello, " + parseLink(parameters)+"!");
+                }
                 builder.append(footer);
-                builder.toString();
                 socket.getOutputStream().write(builder.toString().getBytes("UTF-8"));
+                builder.append(httpResponse);
                 reader.close();
+
+
+//                System.out.println(Arrays.toString(reader.readLine().split("/")));
+//                builder.toString();
             }
         }
+    }
+
+    private static String parseLink(String parameters) {
+        if (parameters.contains("name")) {
+            return parseName(parameters);
+        } else if (parameters.contains("calendar")) {
+            return getCalendar();
+        } else {
+            return "World!";
+        }
+    }
+
+    private static String getCalendar() {
+        return "";
+    }
+
+    private static String parseName(String parameters) {
+        int index = parameters.indexOf("=");
+        index+=1;
+        String result = parameters.substring(index);
+        System.out.println(result);
+        return result.isEmpty() ? "world" : result;
     }
 
 }
